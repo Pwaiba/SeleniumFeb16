c24a2a2a5319f65ee60c7b8853005f32d7bc1531
prernawaiba <prernawaiba@prernas-air.fios-router.home>
2018-02-16 12:25:56 -0500

    Wrote Comment about javaBasics

--- javaBasic/src/demo/car.java
+++ javaBasic/src/demo/car.java
@@ -5,7 +5,7 @@ public class car {
 	public static void main(String[] args) {
 		car meredes = new car();// mercedes is an object of new car.
 	}
-		
+		// This is first concept in javaBasics 
 	}
 
 	
