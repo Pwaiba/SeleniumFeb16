e22e997dda94977bfb23ba3f2fafede49f5c96be
prernawaiba <prernawaiba@prernas-air.fios-router.home>
2018-02-21 20:39:53 -0500

    Added some more functions

--- javaBasic/src/demo/human.java
+++ javaBasic/src/demo/human.java
@@ -35,7 +35,5 @@ Rose.printDelails();
 
 }
 
-
-
 }
 
--- javaBasic/src/inheritance/Security.java
+++ javaBasic/src/inheritance/Security.java
@@ -3,7 +3,11 @@ package inheritance;
 public class Security {
 
 int	bonus = 5000;
-			
-	
+
+		public static void main(String[] args) {
+			Security Hary = new Security();
+			System.out.println("Total yearly bonus =" + Hary.bonus);
+				
+		}
 	
 }
